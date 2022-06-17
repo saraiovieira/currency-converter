7a315c3ec4a5a579170ac3db75a5a39114791f0c
saraiovieira <saravieira09@hotmail.com>
2022-06-17 18:58:13 +0100

    separate project in client and server

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header/Header";
+import CurrencyInput from "./CurrencyInput.js/CurrencyInput";
+
+function App() {
+  return (
+    <>
+        <Header />
+        <CurrencyInput />
+        <CurrencyInput />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/CurrencyInput.js/CurrencyInput.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CurrencyInput = () => {
+  return (
+    <div>
+      <input type="number" className="input" />
+      <select>
+        <option value="Hi">Hi</option>
+      </select>
+    </div>
+  );
+};
+
+export default CurrencyInput;
