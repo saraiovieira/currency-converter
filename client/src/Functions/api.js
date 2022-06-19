8d384f2ffdf1d269af47fdbc7f92e81ff5d6afb5
saraiovieira <saravieira09@hotmail.com>
2022-06-19 19:25:42 +0100

    add functions

---
+++ client/src/Functions/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export async function get(url, data){
+    let config = {
+        baseURL: 'http://localhost:8000',
+        params: data
+    };
+   return await axios.get(url, config);
+}
